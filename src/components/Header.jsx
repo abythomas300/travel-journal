c4ba3174e115c4bca22d2146238d9ddc845f72da
abythomas300 <abythomas300@gmail.com>
2025-09-23 20:22:30 +0530

    feat(style): add header html elements

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import GlobeImage from "./../assets/globe.png"
+
+function Header() {
+    return (
+        <>
+            <img src={GlobeImage} alt="globe image" />
+            <h1>Header here</h1>
+        </>
+    )
+}
+
+export default Header
