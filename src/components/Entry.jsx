808f347bc6cf901a075670a856b62f7e57890a87
abythomas300 <abythomas300@gmail.com>
2025-09-23 23:43:09 +0530

    feat(style): add destination card structure

---
+++ src/components/Entry.jsx
@@ -0,0 +1,18 @@
+
+function Entry() {
+    return(
+        <div className="entry-container">
+            <div className="entry-image">
+                <img src="" alt="destination image" />
+            </div>
+            <div className="entry-content">
+                <div className="destination-metadata"></div>
+                <div className="destination-name"></div>
+                <div className="destination-date"></div>
+                <div className="destination-description"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Entry
