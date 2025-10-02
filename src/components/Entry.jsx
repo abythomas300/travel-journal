9a8f97984062c0d2fca313fda531da67cd8f8231
abythomas300 <abythomas300@gmail.com>
2025-10-02 19:31:46 +0530

    refactor: pass card data as props

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,24 +1,25 @@
-import mountFuji from '../assets/mount-fuji.jpeg'
+function Entry(props) {
 
-function Entry() {
+    const {country, LocationGoogleMapLink, locationName, date, description, img} = props;
+
     return(
         <div className="entry-container">
             <div className="entry-image">
-                <img src={mountFuji} alt="destination image" />
+                <img src={img.imageURL} alt={img.alt} />
             </div>
             <div className="entry-content">
                 <div className="destination-metadata">
-                    <span>Japan</span>
-                    <span><a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View place on Google Map</a></span>
+                    <span>{country}</span>
+                    <span><a href={LocationGoogleMapLink}>View place on Google Map</a></span>
                 </div>
                 <div className="destination-name">
-                    <h2>Mount Fuiji</h2>
+                    <h2>{locationName}</h2>
                 </div>
                 <div className="destination-date">
-                    <p>12 Jan, 2021 - 24 Jan, 2021</p>
+                    <p>{date}</p>
                 </div>
                 <div className="destination-description">
-                    <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    <p>{description}</p>
                 </div>
             </div>
         </div>
